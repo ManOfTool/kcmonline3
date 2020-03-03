b68db31671b34ee7e9df4e716f170e8a144fa40d
john.chang <john.chang@cloudysys.com>
2020-03-03 16:58:20 +0800

    Modify JavaScript function

--- main.js
+++ main.js
@@ -1,48 +1,17 @@
-let IMG_WIDTH = 100;
-let IMG_HEIGHT = 100;
+let IMG_WIDTH = document.getElementById('sw').valueAsNumber;
+let IMG_HEIGHT = document.getElementById('sh').valueAsNumber;
 
-function createCanvas() {
-    let row = document.getElementById('row-select').value;
-    let column = document.getElementById('column-select').value;
-    let canvas = document.getElementById('canvas-dst');
-    canvas.width = IMG_WIDTH * column;
-    canvas.height = IMG_HEIGHT * row;
-
-    document.getElementById('btn-exec').disabled = false;
-}
-
-function drawCanvas(images, type) {
-    let row = document.getElementById('row-select').value;
-    let column = document.getElementById('column-select').value;
-    let c = column;
-
-    let canvas = document.getElementById('canvas-dst');
-    let context = canvas.getContext('2d');
-    let sx = 0, sy = 0;
-    let dx = 0, dy = 0;
-
-    for (let i = 0; i < images.length; i++) {
-        context.drawImage(images[i],
-            sx, sy, IMG_WIDTH, IMG_HEIGHT, dx, dy, IMG_WIDTH, IMG_HEIGHT);
-
-        if ((i + 1) % c > 0) { //
-            dx += IMG_WIDTH;
-        } else { //
-            dx = 0;
-            dy += IMG_HEIGHT;
-        }
-    }
-}
-
-function submit() {
+document.getElementById('file-upload').addEventListener('change', function(){
     let files = document.getElementById('file-upload').files;
-    let fields = document.getElementById('img-field');
-
-    fields.innerHTML = "";
+    // console.log(this.files.length);
 
     if (files.length == 0){
+        document.getElementById('btn-exec').disabled = true;
         alert('No data selected');
     } else {
+        let fields = document.getElementById('img-field');
+        fields.innerHTML = "";
+
         for (let i = 0; i < files.length; i++) {
             let url = window.URL.createObjectURL(files[i]);
             let imgs = `<img class="image-src" src="${url}" style="display: none" />`;
@@ -50,14 +19,49 @@ function submit() {
             fields.innerHTML += imgs;
         }
 
-        document.getElementById('btn-create').disabled = false;
+        document.getElementById('btn-exec').disabled = false;
     }
+});
 
+function createCanvas() {
+    let row = document.getElementById('row-select').value;
+    let column = document.getElementById('column-select').value;
+    let canvas = document.getElementById('canvas-dst');
+
+    canvas.width = IMG_WIDTH * column;
+    canvas.height = IMG_HEIGHT * row;
 }
 
-function goExec() {
-    let images = document.getElementsByClassName('image-src');
-    let type = document.getElementById('draw-type');
+function drawCanvas() {
+    let row = document.getElementById('row-select').value;
+    let column = document.getElementById('column-select').value;
 
-    drawCanvas(images, type);
+    let canvas = document.getElementById('canvas-dst');
+    let context = canvas.getContext('2d');
+    
+    let sx = document.getElementById('sx').valueAsNumber;
+    let sy = document.getElementById('sy').valueAsNumber;
+
+    let dx = 0, dy = 0;
+
+    let images = document.getElementsByClassName('image-src');
+    for (let i = 0; i < images.length; i++) {
+        context.drawImage(images[i],
+            sx, sy, IMG_WIDTH, IMG_HEIGHT,
+            dx, dy, IMG_WIDTH, IMG_HEIGHT);
+        
+        if ((i + 1) % column > 0) {
+            dx += IMG_WIDTH;
+        } else {
+            dx = 0;
+            dy += IMG_HEIGHT;
+        }
+    }
+}
+
+
+function goExec() {
+    createCanvas();
+
+    drawCanvas();
 }
